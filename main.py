999b57e1013055c933a08633bc6e2ed7663cfb0c
Ashish Poudel <ashishpoudel523@gmail.com>
2022-01-06 21:00:28 +0545

    day 2 bootcamp

---
+++ main.py
@@ -0,0 +1,36 @@
+# day = ["Sunday", "Monday", "Tuesday"]
+# a = input("Enter the day:")
+# if a == "Sunday":
+#     print("ghjkl")
+# elif a == "Tuesday":
+#     print("ghjkghjkl.l")
+# else:
+#     print("tyuil;")
+
+# print("out")
+
+
+
+# a = {
+#     "age" : [1, 2, 3],
+#     "address" : "KTM",
+#     "course" : "fdsfdfdsf" 
+
+# }
+
+# print(a.items())
+# print(a.values())
+# print(a.keys())
+
+
+# i = 34
+# while i<=60:
+#     print(i)
+#     i+=1
+
+
+def area(l, b):
+    return l*b
+    print(area(2, 4))
+
+
